7b6573331894c6637fa00339bf8878ef2a65e861
Hitesh <hp5741609@gmail.com>
2023-12-19 16:31:19 +0530

    fix: cors error

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const apiRoutes = require("./router/index");
 const { connectDb } = require("./config/db");
+const cors = require("cors"); 
 require("dotenv").config();
 
 const app = express();
 
 //middleware
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
